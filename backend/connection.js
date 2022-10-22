e90e6fa18708943aa6cdb63dd41653cdc7759102
mock-angel <mockangelbusiness@gmail.com>
2022-10-22 18:24:27 +0530

    switched to a promise connection

--- backend/connection.js
+++ backend/connection.js
@@ -1,9 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+
+export var connection = null;
 
 
-export const connection = mysql.createConnection({
-    host: 'sql6.freemysqlhosting.net',
-    user: 'sql6528423',
-    database: 'sql6528423',
-    password: 'qHWiM9l7qa'
-});
+export const initConnection = async () => {
+
+    connection = await mysql.createConnection({
+        host: 'sql6.freemysqlhosting.net',
+        user: 'sql6528423',
+        database: 'sql6528423',
+        password: 'qHWiM9l7qa'
+    });
+}
+
